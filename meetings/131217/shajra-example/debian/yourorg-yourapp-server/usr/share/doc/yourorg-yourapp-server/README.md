a40c82fb34336a4cecddcffc21abe5705cb9a94e
Sukant Hajra <rrudbskr5g@snkmail.com>
2013-12-17 15:30:38 -0600

    remove ConfigReader
    The ConfigReader work is interesting, but it doesn't really contribute
    to the example, which is intended to be more about build complexity, so
    I cropped it out.  Without it, we still have a multi-module project, but
    it's more minimal.

---
+++ meetings/131217/shajra-example/debian/yourorg-yourapp-server/usr/share/doc/yourorg-yourapp-server/README.md
@@ -0,0 +1,5 @@
+Example Project to Illustrate SBT Practices
+===========================================
+
+This project is an example project to illustrate some practices with SBT to
+help keep the build clean and expressive.
--- meetings/131217/shajra-example/modules/extn/typesafeconfig/src/main/scala/package.scala
+++ meetings/131217/shajra-example/modules/extn/typesafeconfig/src/main/scala/package.scala
@@ -1,15 +1,5 @@
 package org.yourorg.extn
 
 
-import com.typesafe.config.ConfigException
-
-import org.yourorg.extn.scalaz.ReaderValidationNel
-
-
 /** Functionality to assist working with the Typesafe Config library. */
-package object typesafeconfig {
-
-  type ConfigReader[A] = ReaderValidationNel[Configuration, A, ConfigException]
-
-
-}
+package object typesafeconfig
